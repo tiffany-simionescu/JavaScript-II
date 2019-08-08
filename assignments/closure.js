2d1263ac3786c8ee33294c35794db3c32b2181c6
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-08-07 23:21:47 -0400

    Finished the STRETCH exercises in closure.js file

--- assignments/closure.js
+++ assignments/closure.js
@@ -14,14 +14,36 @@ sayHello();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return ++count;
+  };
 };
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: function() {
+      return ++count;
+    },
+    decrement: function() {
+      return --count;
+    }
+  };
 };
+
+const myCounterFactory = counterFactory();
+
+console.log(myCounterFactory.increment());
+console.log(myCounterFactory.increment());
+console.log(myCounterFactory.decrement());
+console.log(myCounterFactory.decrement());
